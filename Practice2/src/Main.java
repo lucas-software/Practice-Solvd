2b782b7afc3949ccd4cdec0044521c16c0e56857
lucas-software <160547725+lucas-software@users.noreply.github.com>
2024-08-26 23:41:07 -0300

    Practice

--- Practice2/src/Main.java
+++ Practice2/src/Main.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Nba nba = new Nba(1,"Basketball",32);
-        Nfl nfl = new Nfl(2,"Football");
-        NbaTeam nbaTeam = new NbaTeam("Brazillian Angels",Conference.NORTH,1);
-        NflTeam nflTeam = new NflTeam("Brazillian Devils",Conference.SOUTH,2);
-        Player Neymar = new Player(11,"Brazil","Forward");
+        Nba nba = new Nba(1, "Basketball", 32);
+        Nfl nfl = new Nfl(2, "Football");
+        NbaTeam nbaTeam = new NbaTeam("Brazillian Angels", Conference.NORTH, 1);
+        NflTeam nflTeam = new NflTeam("Brazillian Devils", Conference.SOUTH, 2);
+        Player Neymar = new Player(11, "Brazil", "Forward");
         ArrayList<Staff> staffers = new ArrayList<>();
-        Coach headCoach = new Coach(1,"Bill Belichick",72);
+        Coach headCoach = new Coach(1, "Bill Belichick", 72);
         staffers.add(headCoach);
         StaffMoves.hireStaff();
-        Coordinator coordinator = new Coordinator(2,"Defensive coordinator","Jerod Mayo",38);
+        Coordinator coordinator = new Coordinator(2, "Defensive coordinator", "Jerod Mayo", 38);
         staffers.add(coordinator);
         StaffMoves.hireStaff();
-        Worker worker = new Worker(100,"Cleaning");
+        Worker worker = new Worker(100, "Cleaning");
         staffers.add(worker);
         StaffMoves.hireStaff();
         //polymorfism with the abstract class Staff
-        Staff staffer = new Worker(101,"Cashier");
+        Staff staffer = new Worker(101, "Cashier");
         staffers.add(staffer);
         StaffMoves.hireStaff();
-        Staff staffer2 = new Coach(2,"Mike Vrabel",51);
+        Staff staffer2 = new Coach(2, "Mike Vrabel", 51);
         staffers.add(staffer2);
         StaffMoves.hireStaff();
         //methods toString,hashCode and equals
@@ -37,7 +37,7 @@ public class Main {
         headCoach.work();
         headCoach.enrollInsurance();
         headCoach.receiveSalary();
-        Intern intern = new Intern(6,120);
+        Intern intern = new Intern(6, 120);
         intern.work();
         intern.attendTraining();
         intern.receiveSalary();
@@ -46,20 +46,28 @@ public class Main {
 
         //Try-catch use
         try {
-            for(Staff i : staffers) {
-                if(coordinator.getId() == 19191) {
+            for (Staff i : staffers) {
+                if (coordinator.getId() == 19191) {
                     System.out.println("Exists");
                 } else {
                     throw new StaffNotFoundException("Id not found");
-                    throw new StaffRemovalException("Could not remove the Staffer");
                 }
             }
         } catch (StaffNotFoundException e) {
             System.out.println("Staffer not found: " + e.getMessage());
-        } catch (StaffRemovalException e) {
-            System.out.println("Error removing staffer: " + e.getMessage());
+
+            try {
+                for (Staff i : staffers) {
+                    if (coordinator.getId() == 19191) {
+                        System.out.println("Exists");
+                    } else {
+                        throw new StaffRemovalException("Could not remove the Staffer");
+                    }
+                }
+            } catch (StaffRemovalException r) {
+                System.out.println("Error removing staffer: " + e.getMessage());
+            }
+
         }
-
-
     }
 }
